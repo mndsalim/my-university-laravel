0a234a3b686494e2bfd9a72557aa159827a79634
mndsalim <mohanedsalim@gmail.com>
2025-09-10 13:30:45 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('dashboard');
+})
+->middleware(['auth'])
+->name('dashboard');
+
+
+Route::get('login', function () {
+    return view('auth.login');
+})
+->middleware('guest')
+->name('login');
