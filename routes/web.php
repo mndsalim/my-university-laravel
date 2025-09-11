1213873d3d89a057340eb8df2d205afff013c666
mndsalim <mohanedsalim@gmail.com>
2025-09-11 13:12:30 +0200

    dashboard layouts configured

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('pages.dashboard');
 })
 // ->middleware(['auth'])
 ->name('dashboard');
